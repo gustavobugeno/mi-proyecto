50d62672556f36ef2085934914e1b1e6d04e822b
gustavo bugueño <cb.bugueno@hotmail.com>
2026-07-05 23:59:48 -0400

    feat: barra de búsqueda y filtrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { useFetch } from "./hooks/useFetch";
 import List from "./components/List";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   const { data, loading, error } = useFetch("https://ghibliapi.vercel.app/films");
+  const [searchTerm, setSearchTerm] = useState("");
 
   if (loading) return <p>Cargando películas...</p>;
   if (error) return <p>Error al cargar API: {error}</p>;
 
+  const filtered = data.filter((item) =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Studio Ghibli</h1>
-      <List items={data} />
+
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <List items={filtered} />
     </div>
   );
 }
